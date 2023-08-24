3e0bb0f8a73ccd0a7afb0ab784a34506ab6d5bca
AgusMartino <martinoagustin4@gmail.com>
2023-08-24 08:52:10 -0300

    Login 2.0

--- src/main.js
+++ src/main.js
@@ -1,27 +1,14 @@
 import { createApp } from 'vue'
-import App from './index.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
+loadFonts()
 
-const routes = [
-  {
-    path:'/',
-    name:'login',
-    component: () => import('./views/Login.vue')
-  },
-  {
-    path:'/menuPrincipal',
-    name:'menuPrincipal',
-    component: () => import('./views/menuPrincipal.vue')
-  }
-
-]
-
-const router = createRouter({
-  history: createWebHistory('/'),
-  routes
-})
-
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
